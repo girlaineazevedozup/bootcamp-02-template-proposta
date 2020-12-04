caa5853e950ed1246b3600b746aeeecffac8df2e
girlaine.azevedo <girlaine.azevedo@zup.com.br>
2020-12-04 12:40:46 -0300

    Docker

--- src/main/java/com/zup/bootcamp/client/CartaoClient.java
+++ src/main/java/com/zup/bootcamp/client/CartaoClient.java
@@ -2,7 +2,6 @@ package com.zup.bootcamp.client;
 
 import com.zup.bootcamp.client.response.CartaoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
